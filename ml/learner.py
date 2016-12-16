8a2cb28360a2bc0a179f6193790df608ecf37df0
Federico Recanati <federico.recanati@arm.com>
2016-12-16 14:58:58 +0100

    x
    Change-Id: Ib3b6756aba6222dbd382220121e0c3fc68995514

--- ml/learner.py
+++ ml/learner.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib
 matplotlib.rcParams['backend'] = "Qt4Agg"
 import matplotlib.pyplot as plt
+import sklearn.neighbors as sk_neigh
 
 class LinRegLearner:
     def __init__(self):
@@ -14,6 +15,16 @@ class LinRegLearner:
     def query(self, x):
         return np.polyval(self.p, x)
 
+class KNNLearner:
+    def __init__(self, n):
+        self.knn = sk_neigh.KNeighborsRegressor(n_neighbors=n)
+
+    def train(self, x, y):
+        self.knn.fit(x.reshape(-1, 1), y)
+
+    def query(self, x):
+        return self.knn.predict(x.reshape(-1, 1))
+
 if __name__ == "__main__":
     x = np.arange(20)
     y = np.arange(20) + np.random.rand(20) - .5
@@ -22,5 +33,13 @@ if __name__ == "__main__":
     lr.train(x, y)
     y_h = lr.query(x)
 
+    plt.subplot(211)
     plt.plot(x, y, 'ob', x, y_h, '-r')
+
+    lr = KNNLearner(3)
+    lr.train(x,y)
+    y_h = lr.query(np.arange(100.0)/5)
+
+    plt.subplot(212)
+    plt.plot(x, y, 'ob', np.arange(100.0)/5, y_h, 'o-r')
     plt.show()
